9ee533d362479d32d636c1165cdf14681d5610d0
Uciel Sola <solauciel@gmail.com>
2022-01-21 00:43:48 -0300

    test

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -34,7 +34,7 @@
 		position: absolute;
 		width: 100%;
 		height: 100%;
-		background-image: url('./images/cover.webp');
+		background-image: var(--bg-image);
 		background-position: center;
 		background-size: cover;
 		border-bottom-left-radius: 1rem;
