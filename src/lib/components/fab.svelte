9ebfb06d5deaca8925d6b3808987c0c915dc73e9
Uciel Sola <solauciel@gmail.com>
2022-01-22 03:27:37 -0300

    fab button styles

--- src/lib/components/fab.svelte
+++ src/lib/components/fab.svelte
@@ -2,8 +2,11 @@
 	export let percentage = 0;
 </script>
 
-<a href="/reservas" class={percentage > 85 ? 'pushed-up' : ''}><button>CONSULTAR RESERVA</button></a
->
+<a href="/reservas" class={percentage > 85 ? 'pushed-up' : ''}
+	><div class="wrapper">
+		<button>CONSULTAR RESERVA</button>
+	</div>
+</a>
 
 <style>
 	a {
@@ -12,5 +15,38 @@
 	a.pushed-up {
 		margin-bottom: var(--footer-height);
 		transition: margin-bottom 0.5s var(--easing);
+	}
+	.wrapper {
+		background: #00000000;
+		border-radius: 10px;
+		padding: 3px;
+		transition: all 0.35s var(--easing);
+		background: linear-gradient(180deg, #e2504c 0%, #fbc05d 100%);
+	}
+
+	button {
+		opacity: 0.8;
+		background-color: #ffffff00;
+		padding-block: 1rem;
+		width: 75vw;
+		max-width: 15rem;
+		border: none;
+		border-radius: 10px;
+		color: #fff;
+		transition: all 0.35s var(--easing);
+	}
+
+	button:active {
+		opacity: 1;
+		background-color: #fff;
+		color: var(--primary-color);
+		transition: all 0.35s var(--easing);
+	}
+
+	@media (hover: hover) {
+		button:hover {
+			opacity: 1;
+			transition: all 0.35s var(--easing);
+		}
 	}
 </style>
--- src/routes/galeria.svelte
+++ src/routes/galeria.svelte
@@ -1,10 +1,24 @@
-<section id="interiores-1">asd</section>
-<section id="interiores-2">asd</section>
-<section id="exteriores">asd</section>
-
+<section id="interiores-1">
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias eveniet dignissimos adipisci
+	voluptas corrupti doloribus eius nobis quae rem, cumque ipsam quas, consequuntur hic. Rerum at
+	quos in veniam aliquid rem quod omnis quibusdam esse deserunt. Reiciendis saepe quod placeat, sunt
+	ipsa doloremque inventore vitae id mollitia quae, similique ratione.
+</section>
+<section id="interiores-2">
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias eveniet dignissimos adipisci
+	voluptas corrupti doloribus eius nobis quae rem, cumque ipsam quas, consequuntur hic. Rerum at
+	quos in veniam aliquid rem quod omnis quibusdam esse deserunt. Reiciendis saepe quod placeat, sunt
+	ipsa doloremque inventore vitae id mollitia quae, similique ratione.
+</section>
+<section id="exteriores">
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias eveniet dignissimos adipisci
+	voluptas corrupti doloribus eius nobis quae rem, cumque ipsam quas, consequuntur hic. Rerum at
+	quos in veniam aliquid rem quod omnis quibusdam esse deserunt. Reiciendis saepe quod placeat, sunt
+	ipsa doloremque inventore vitae id mollitia quae, similique ratione.
+</section>
 
 <style>
-    section {
-        min-height: 40vw;
-    }
+	section {
+		min-height: 40vw;
+	}
 </style>
