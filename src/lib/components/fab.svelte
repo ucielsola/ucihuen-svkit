f4801b0122655c9e1984cd28647e1bd3cfcbff69
Uciel Sola <solauciel@gmail.com>
2022-01-22 03:32:42 -0300

    fab styles improved

--- src/lib/components/fab.svelte
+++ src/lib/components/fab.svelte
@@ -1,10 +1,20 @@
 <script>
 	export let percentage = 0;
+	export let width;
+	let active = false;
+	const toggleActive = () => {
+		console.log(width);
+		active = !active;
+	};
 </script>
 
 <a href="/reservas" class={percentage > 85 ? 'pushed-up' : ''}
-	><div class="wrapper">
-		<button>CONSULTAR RESERVA</button>
+	><div class="wrapper {active ? 'active' : ''}">
+		{#if width > 1024}
+			<button on:mousedown={toggleActive} on:mouseup={toggleActive}>CONSULTAR RESERVA</button>
+		{:else}
+			<button on:touchstart={toggleActive} on:touchend={toggleActive}>CONSULTAR RESERVA</button>
+		{/if}
 	</div>
 </a>
 
@@ -17,21 +27,26 @@
 		transition: margin-bottom 0.5s var(--easing);
 	}
 	.wrapper {
+		opacity: 0.8;
 		background: #00000000;
 		border-radius: 10px;
 		padding: 3px;
 		transition: all 0.35s var(--easing);
 		background: linear-gradient(180deg, #e2504c 0%, #fbc05d 100%);
 	}
+	.wrapper.active {
+		opacity: 1;
+		transition: all 0.35s var(--easing);
+	}
 
 	button {
-		opacity: 0.8;
 		background-color: #ffffff00;
-		padding-block: 1rem;
+		padding-block: 0.5rem;
 		width: 75vw;
 		max-width: 15rem;
 		border: none;
 		border-radius: 10px;
+		font-size: 1.2rem;
 		color: #fff;
 		transition: all 0.35s var(--easing);
 	}
