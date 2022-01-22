d0e0124802939f68f35a985be79fcc2e70529c58
Uciel Sola <solauciel@gmail.com>
2022-01-22 16:28:33 -0300

    added post form

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -1,34 +1,20 @@
 <script>
-	let key = import.meta.env.VITE_API_KEY;
+	let email;
 </script>
 
 <form action="https://api.staticforms.xyz/submit" method="post">
-	<!-- Replace with accesKey sent to your email -->
 	<input type="hidden" name="accessKey" value="e88d957a-8d40-4776-bde4-1453632cbc60" />
-	<!-- Required -->
 	<label for="name">Nombre</label>
 	<input type="text" name="name" />
-	<!-- Optional -->
 	<label for="subject">Asunto</label>
 	<input type="text" name="subject" />
-	<!-- Optional -->
 	<label for="email">Mail</label>
-	<input type="text" name="email" />
-	<!-- Optional -->
+	<input type="text" name="email" bind:value={email} />
 	<label for="phone">Tel</label>
 	<input type="number" name="phone" />
-	<!-- Optional -->
 	<label for="message">Mensaje</label>
 	<textarea name="message" />
-	<!-- Optional -->
-	<!-- If you want replyTo to be set to specific email -->
-	<input type="text" name="replyTo" value="myreplytoemail@example.com" />
-	<!-- Optional -->
-	<!-- Specify @ as reply to value if you want it to be customers email -->
-	<input type="hidden" name="replyTo" value="@" />
-	<!-- Optional -->
-	<!-- If you want form to redirect to a specific url after submission -->
+	<input type="hidden" name="replyTo" bind:value={email} />
 	<input type="hidden" name="redirectTo" value="https://ucihuen.vercel.app/gracias" />
-	<!-- Optional -->
 	<input type="submit" value="Submit" />
 </form>
