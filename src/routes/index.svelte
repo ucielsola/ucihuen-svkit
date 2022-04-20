9810e14f71c20be84b7699e7311c41f9fb38b1ce
Uciel Sola <solauciel@gmail.com>
2022-04-20 15:58:38 -0300

    increased text contrast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -176,6 +176,8 @@
 			padding-block: 1rem;
 			padding-inline: 1.5rem;
 			border-radius: 10px;
+			font-weight: 900;
+			font-size: 1.2rem;
 			transition: background-color 0.35s var(--easing);
 		}
 		.vertical-banner a:active {
