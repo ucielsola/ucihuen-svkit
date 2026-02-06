cd60f0f2b30009196f33616709760a404c0ff6e8
Uciel Sola <solauciel@gmail.com>
2026-02-06 14:47:57 -0300

    migrate 4

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DBXcMi6C.js","_app/immutable/chunks/DSduQYlM.js","_app/immutable/chunks/BC4EkEQe.js","_app/immutable/chunks/kckne7wd.js","_app/immutable/chunks/7PdrsQZO.js","_app/immutable/chunks/DnP7PCZ_.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.C5Ap4pvl.js","_app/immutable/chunks/DSduQYlM.js","_app/immutable/chunks/BC4EkEQe.js","_app/immutable/chunks/DyxHvis7.js","_app/immutable/chunks/DxKN-uii.js","_app/immutable/chunks/kckne7wd.js","_app/immutable/chunks/qIX1-8Og.js","_app/immutable/chunks/Bdl1seu_.js","_app/immutable/chunks/CN1uJvll.js"];
-export const stylesheets = ["_app/immutable/assets/swiper.DH8nk603.css","_app/immutable/assets/2.BFOTXBIx.css"];
-export const fonts = [];
