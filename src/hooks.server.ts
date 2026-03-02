e686079dfe5b562f00c6a9e9ef5736f4661582ea
Uciel Sola <solauciel@gmail.com>
2026-03-02 20:11:03 -0300

    redirect wrong paths

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
 import { paraglideMiddleware } from '$lib/paraglide/server';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
+
+const paths = ['/', '/galeria', '/paseos'];
 
 export const handle: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+		if (!paths.includes(event.url.pathname)) return redirect(307, '/');
+
 		event.request = localizedRequest;
 		return resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%lang%', locale)
